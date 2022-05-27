f7d2e92c12f551eee59e85c481ee02c4cffa6d70
Choi Ji Hun <100457604+Apocachip@users.noreply.github.com>
2022-05-27 10:40:57 +0900

    add

--- app_eda.py
+++ app_eda.py
@@ -3,19 +3,30 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
 
 df = pd.read_csv('data/GPUs.csv')
+img = Image.open('data/GPU.jpg')
+img_mask = np.array(img)
+gpu_names = ' '.join(i for i in df['productName'].astype(str))
+stopwords = set(STOPWORDS)
+wc = WordCloud(background_color='white', mask= img_mask, stopwords= stopwords).generate(gpu_names)
 
 def run_eda() :
-    st.title('년도별 그래픽카드 성능 증가율')
+    st.title('탐색적 데이터 분석')
     
     if st.button('전체 데이터 보기') :
         st.dataframe(df)
         if st.button('닫기') :
             st.text('')
 
-    menu = df['releaseYear'].unique().tolist()
-
-    st.multiselect('년도를 골라주세요(최소2개)', menu)
-
-    
+    if st.button('워드클라우드 보기') :
+        plt.figure(figsize= (10, 6))
+        plt.imshow(wc)
+        plt.tight_layout(pad=0)
+        plt.axis('off')
+        plt.show()
+        st.pyplot()
+        if st.button('닫기') :
+            st.text(' ')
