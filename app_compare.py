bf127bad8b90489a455d8ea0df369ed12f852b87
Choi Ji Hun <100457604+Apocachip@users.noreply.github.com>
2022-05-27 12:57:10 +0900

    add

--- app_compare.py
+++ app_compare.py
@@ -8,37 +8,37 @@ def run_compare() :
     df = pd.read_csv('data/GPU.csv')
 
     m_columns = df['manufacturer'].unique()
-    menu = m_columns.tolist()
+    col_menu1 = m_columns.tolist()
     
-    choice_manufacturer = st.selectbox('제조사 선택', menu)
-
+    choice_manufacturer = st.selectbox('제조사 선택', col_menu1)
+    
     if choice_manufacturer :
         st.text(choice_manufacturer + '을/를 선택하셨습니다.')
 
-        menu2 = df.loc[df['manufacturer'].str.contains(choice_manufacturer)]['productName']
+        name_menu1 = df.loc[df['manufacturer'].str.lower() == choice_manufacturer.lower()]['productName']
         
-        choice_graphic_card = st.selectbox('그래픽카드 선택', menu2)
+        choice_graphic_card = st.selectbox('그래픽카드 선택', name_menu1)
 
         if choice_graphic_card :
-            now_product = df.loc[df['manufacturer'].str.contains(choice_manufacturer)]
+            now_product = df.loc[df['manufacturer'].str.lower() == choice_manufacturer.lower()]
 
-            product_choice = now_product.loc[now_product['productName'].str.contains(choice_graphic_card)]
+            product_choice = now_product.loc[now_product['productName'].str.lower() == choice_graphic_card.lower()]
 
             st.dataframe(product_choice)
 
-            choice_manufacturer2 = st.selectbox('제조사2 선택', menu)
+    choice_manufacturer2 = st.selectbox('제조사2 선택', col_menu1)
 
     if choice_manufacturer2 :
-        st.text(choice_manufacturer2 + '을/를 선택하셨습니다')
+        st.text(choice_manufacturer2 + '을/를 선택하셨습니다.')
 
-        menu3 = df.loc[df['manufacturer'].str.contains(choice_manufacturer2)]['productName']
+        name_menu2 = df.loc[df['manufacturer'].str.lower() == choice_manufacturer2.lower()]['productName']
 
-        choice_graphic_card2 = st.selectbox('그래픽카드 선택2', menu3)
+        choice_graphic_card2 = st.selectbox('그래픽카드 선택2', name_menu2)
 
         if choice_graphic_card2 :
-            now_product2 = df.loc[df['manufacturer'].str.contains(choice_manufacturer2)]
+            now_product2 = df.loc[df['manufacturer'].str.lower() == choice_manufacturer2.lower()]
 
-            product_choice2 = now_product2.loc[now_product2['productName'].str.contains(choice_graphic_card2)]
+            product_choice2 = now_product2.loc[now_product2['productName'].str.lower() == choice_graphic_card2.lower()]
 
             st.dataframe(product_choice2)
 
