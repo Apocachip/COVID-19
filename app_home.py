6091d4e6d7dbb85d180c386aa50f1d1a06d3c9ef
Choi Ji Hun <100457604+Apocachip@users.noreply.github.com>
2022-06-08 17:29:44 +0900

    add

--- app_home.py
+++ app_home.py
@@ -2,7 +2,4 @@ import streamlit as st
 
 def run_home() :
     st.title('그래픽카드 비교')
-    st.subheader('각 제조사별로 그래픽카드를 비교하여 볼수 있는 앱')
-
-    st.text('productNmae - 해당하는 제조사의 그래픽카드 이름 (제품이름)')
-    st.text('releaseYear - 그래픽카드의 출시일 또는 예정일')
+    st.subheader('NVIDIA & AMD의 그래픽카드를 비교하여 볼수 있는 앱')
